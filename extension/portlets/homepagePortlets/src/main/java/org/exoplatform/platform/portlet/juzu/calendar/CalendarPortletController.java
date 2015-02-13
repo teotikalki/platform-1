ae95f7eb12c45ffa0618a3773ec971f4907ed3ae
mzorai <mzorai@exoplatform.com>
2015-02-13 12:11:15 +0100

    PLF-5846:Date Label of Calendar gadget on Homepage is not correct

--- extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/calendar/CalendarPortletController.java
+++ extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/calendar/CalendarPortletController.java
@@ -43,6 +43,7 @@ import org.gatein.common.text.EntityEncoder;
 
 import javax.inject.Inject;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Calendar;
 
@@ -134,6 +135,27 @@ public class CalendarPortletController {
         container.render();
     }
 
+        // Format the Date pattern
+    private String formatDate(Locale locale) {
+        String datePattern = "";
+        DateFormat dateFormat = SimpleDateFormat.getDateInstance(DateFormat.SHORT, locale);
+        // convert to unique pattern
+        datePattern = ((SimpleDateFormat)dateFormat).toPattern();
+        if (!datePattern.contains("yy")) {
+        datePattern = datePattern.replaceAll("y", "yy");
+        }
+        if (!datePattern.contains("yyyy")) {
+        datePattern = datePattern.replaceAll("yy", "yyyy");
+        }
+        if (!datePattern.contains("dd")) {
+        datePattern = datePattern.replaceAll("d", "dd");
+        }
+        if (!datePattern.contains("MM")) {
+        datePattern= datePattern.replaceAll("M", "MM");
+        }
+        return datePattern;
+        }
+
     @Ajax
     @Resource
     public void calendarHome() throws Exception {
@@ -145,7 +167,8 @@ public class CalendarPortletController {
         String date_act = null;
         String username = RequestContext.getCurrentInstance().getRemoteUser();
         Locale locale =  Util.getPortalRequestContext().getLocale();
-        DateFormat d = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+        String dp= formatDate(locale);
+        DateFormat d = new SimpleDateFormat(dp);
         DateFormat dTimezone = DateFormat.getDateInstance(DateFormat.SHORT, locale);
         dTimezone.setCalendar(CalendarPortletUtils.getCurrentCalendar());
         Long date = new Date().getTime();
@@ -165,11 +188,7 @@ public class CalendarPortletController {
         // Put it back in the Date object
         currentTime = cal.getTime();
         date_act = d.format(currentTime);
-        String delim =  getDateDelimiter(date_act);
-        if(delim != null && date_act.indexOf(delim) < 2 ) {
-             date_act = new StringBuffer("0").append(date_act).toString();
-               }
-         Date comp = currentTime;
+        Date comp = currentTime;
         HashMap parameters = new HashMap();
         String defaultCalendarLabel = "Default";
         String dateLabel = "";
